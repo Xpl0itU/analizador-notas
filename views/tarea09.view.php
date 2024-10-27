6bc64cd34270cb5b3176a892f389f10b5454d96d
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-10-27 13:22:23 +0100

    Show table correctly

--- views/tarea09.view.php
+++ views/tarea09.view.php
@@ -1,65 +1,68 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Tarea 09</h1>
-
 </div>
 
 <!-- Content Row -->
 <?php if (isset($data['resultado'])) { ?>
     <div class="col-12 col-lg-6">
         <div>
-
         </div>
     </div>
     <div class="row">
         <div class="col-12">
             <div class="alert alert-success">
                 <table class="table table-striped table-bordered">
-                    <?php foreach ($data['resultado']['tabla'] as $key => $value) { ?>
-                        <tr>
-                            <th>Asignatura</th>
+                    <thead>
+                    <tr>
+                        <th>Categoría</th>
+                        <?php foreach ($data['resultado']['tabla'] as $key => $value) { ?>
                             <th><?php echo $key ?></th>
-                        </tr>
-                        <tr>
-                            <th>Media</th>
+                        <?php } ?>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th>Media</th>
+                        <?php foreach ($data['resultado']['tabla'] as $value) { ?>
                             <td><?php echo $value['media'] ?></td>
-                        <tr>
-                            <th>Suspensos</th>
-                        <tr>
+                        <?php } ?>
+                    </tr>
+                    <tr>
+                        <th>Suspensos</th>
+                        <?php foreach ($data['resultado']['tabla'] as $value) { ?>
                             <td><?php echo count($value['suspensos']) ?></td>
-                        </tr>
-                        <tr>
-                            <th>Aprobados</th>
-                        <tr>
+                        <?php } ?>
+                    </tr>
+                    <tr>
+                        <th>Aprobados</th>
+                        <?php foreach ($data['resultado']['tabla'] as $value) { ?>
                             <td><?php echo count($value['aprobados']) ?></td>
-                        </tr>
-                        </tr>
-                        <tr>
-                            <th>Máxima nota</th>
+                        <?php } ?>
+                    </tr>
+                    <tr>
+                        <th>Máxima nota</th>
+                        <?php foreach ($data['resultado']['tabla'] as $value) { ?>
                             <td><?php echo $value['max']['alumno'] . ': ' . $value['max']['nota'] ?></td>
-                        </tr>
-                        <tr>
-                            <th>Mínima nota</th>
+                        <?php } ?>
+                    </tr>
+                    <tr>
+                        <th>Mínima nota</th>
+                        <?php foreach ($data['resultado']['tabla'] as $value) { ?>
                             <td><?php echo $value['min']['alumno'] . ': ' . $value['min']['nota'] ?></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <hr/>
-                            </td>
-                        </tr>
-                    <?php } ?>
+                        <?php } ?>
+                    </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
     </div>
-    <?php
-}
-?>
+<?php } ?>
+
 <div class="row">
     <div class="col-12">
         <div class="card shadow mb-4">
-            <div
-                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Analizar notas en JSON</h6>
             </div>
             <div class="card-body">
@@ -73,4 +76,3 @@
         </div>
     </div>
 </div>
-
